ae5219a7efd275529cc6077fc8ccc4b82262aa20
bhuang95 <huangbo9955@gmail.com>
2022-01-19 17:30:00 +0000

    Update more to less

--- dr-work/job_rocotostat_DACycle-dr-data-backup.sh
+++ dr-work/job_rocotostat_DACycle-dr-data-backup.sh
@@ -3,6 +3,6 @@ module load rocoto
 
 
 # NRT run for GBBEPx bin2nc conversion 
-rocotostat -w /home/Bo.Huang/JEDI-2020/GSDChem_cycling/global-workflow-CCPP2-Chem-NRT-clean/dr-work/NRT-DACycle-dr-data-backup.xml -d /scratch2/BMC/gsd-fv3-dev/MAPP_2018/bhuang/JEDI-2020/JEDI-FV3/expRuns/global-workflow-CCPP2-Chem-NRT-clean/dr-work/NRT-DACycle-dr-data-backup.db | more  
+rocotostat -w /home/Bo.Huang/JEDI-2020/GSDChem_cycling/global-workflow-CCPP2-Chem-NRT-clean/dr-work/NRT-DACycle-dr-data-backup.xml -d /scratch2/BMC/gsd-fv3-dev/MAPP_2018/bhuang/JEDI-2020/JEDI-FV3/expRuns/global-workflow-CCPP2-Chem-NRT-clean/dr-work/NRT-DACycle-dr-data-backup.db | less 
 #rocotostat_log.out
 
--- dr-work/job_rocotostat_prepAODOBS-MODIS.sh
+++ dr-work/job_rocotostat_prepAODOBS-MODIS.sh
@@ -3,6 +3,6 @@ module load rocoto
 
 
 # NRT run for GBBEPx bin2nc conversion 
-rocotostat -w /home/Bo.Huang/JEDI-2020/GSDChem_cycling/global-workflow-CCPP2-Chem-NRT-clean/dr-work/NRT-prepAODOBS-MODIS.xml -d /scratch2/BMC/gsd-fv3-dev/MAPP_2018/bhuang/JEDI-2020/JEDI-FV3/expRuns/global-workflow-CCPP2-Chem-NRT-clean/dr-work/NRT-prepAODOBS-MODIS.db | more  
+rocotostat -w /home/Bo.Huang/JEDI-2020/GSDChem_cycling/global-workflow-CCPP2-Chem-NRT-clean/dr-work/NRT-prepAODOBS-MODIS.xml -d /scratch2/BMC/gsd-fv3-dev/MAPP_2018/bhuang/JEDI-2020/JEDI-FV3/expRuns/global-workflow-CCPP2-Chem-NRT-clean/dr-work/NRT-prepAODOBS-MODIS.db | less 
 #rocotostat_log.out
 

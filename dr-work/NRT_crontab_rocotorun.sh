d3e898ee866855c4bfaf8539676a047b1ce87253
bhuang95 <huangbo9955@gmail.com>
2022-02-15 22:29:53 +0000

    JEDI module restored after Hera breakdown

--- dr-work/NRT_crontab_rocotorun.sh
+++ dr-work/NRT_crontab_rocotorun.sh
@@ -20,7 +20,7 @@ echo "NRT run for GBBEPx bin2nc conversion"
 /apps/rocoto/1.3.3/bin/rocotorun -w /home/Bo.Huang/JEDI-2020/GSDChem_cycling/global-workflow-CCPP2-Chem-NRT-clean/dr-work/NRT-genGBBEPx.xml -d /scratch2/BMC/gsd-fv3-dev/MAPP_2018/bhuang/JEDI-2020/JEDI-FV3/expRuns/global-workflow-CCPP2-Chem-NRT-clean/dr-work/NRT-genGBBEPx.db
 
 # NRT run for GDAS analysis and sfc processing
-#echo "NRT run for GDAS analysis and sfc processing"
+echo "NRT run for GDAS analysis and sfc processing"
 /apps/rocoto/1.3.3/bin/rocotorun -w /home/Bo.Huang/JEDI-2020/GSDChem_cycling/global-workflow-CCPP2-Chem-NRT-clean/dr-work/NRT-prepGdasAnalSfc.xml -d /scratch2/BMC/gsd-fv3-dev/MAPP_2018/bhuang/JEDI-2020/JEDI-FV3/expRuns/global-workflow-CCPP2-Chem-NRT-clean/dr-work/NRT-prepGdasAnalSfc.db
 
 # NRT run for DA cycling 

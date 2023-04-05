88aad3d04e8dc87cd9639a95613bb28cb0eba473
bhuang95 <huangbo9955@gmail.com>
2023-04-05 18:02:04 +0000

    Updated

--- misc/job_changePermission.sh
+++ misc/job_changePermission.sh
@@ -3,7 +3,10 @@
 DIR1=/scratch1/BMC/gsd-fv3-dev/MAPP_2018/bhuang/JEDI-2020/JEDI-FV3/NRTdata/gdasAna/C96
 DIR2=/scratch2/BMC/gsd-fv3-dev/MAPP_2018/bhuang/JEDI-2020/JEDI-FV3/expRuns/global-workflow-CCPP2-Chem-NRT-clean/dr-data
 
-echo "Change permission for ${DIR1}"
-chmod -R 777 ${DIR1}/*
-echo "Change permission for ${DIR2}"
-chmod -R 777 ${DIR2}/*
+DIRS="${DIR1} ${DIR2}"
+
+for DIR in ${DIRS}; do
+    echo "Change permission for ${DIR}"
+    chmod -R 777 ${DIR}
+    chmod g-s ${DIR}
+done
